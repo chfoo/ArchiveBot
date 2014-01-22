59df4a31c538af78ff20dedfe6478cf0b59d52f5
Christopher Foo <chris.foo@gmail.com>
2014-01-22 16:37:18 -0500

    pipeline.py: Requires Wpull 0.9.2.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -24,7 +24,7 @@ from seesaw.util import find_executable
 VERSION = "20140122.01-wpull"
 USER_AGENT = "ArchiveTeam ArchiveBot/%s" % VERSION
 EXPIRE_TIME = 60 * 60 * 48  # 48 hours between archive requests
-WPULL_EXE = find_executable('Wpull', "0.9.1",
+WPULL_EXE = find_executable('Wpull', "0.9.2",
         [ './wpull' ])
 
 if not WPULL_EXE:
